a2df3a9e5aa0d0da11bdc2793d0dd6450b238003
RochmatBasukiXD <103634965+RozhBasXYZ@users.noreply.github.com>
2022-11-09 21:59:18 +0700

    Update run.py

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
 import os
-from instarozh import makedirectory as main
-main()
+from babas.instarozh import makedirectory as main
+from babas.instarozh32 import makedirectory as main2
+
+
+try: main()
+except: main2()
