0240a0503d5fcf90c7344829d066b2e8d2746ca2
RochmatBasukiXD <103634965+RozhBasXYZ@users.noreply.github.com>
2022-11-09 21:22:33 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 import os
-from instarozh64 import makedirectory as run64
+from babas import makedirectory as run64
 
+if "64" in str(os.system("uname -m")):
+   run64()
+else:
+   run32()
 
-
-run64()
