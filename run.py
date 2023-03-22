f659f8e57cdf5fc555d395e94f761931c8e3ae93
Lala Lala Haha HiHi <103634965+RozhBasXYZ@users.noreply.github.com>
2023-03-22 20:26:58 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,3 @@
 import os
-
-try:
-   from data.instarozh import makedirectory as main
-   main()
-except:
-   from data.instarozh32 import makedirectory as main2
-   main2()
+from data.instarozh import makedirectory as main
+main()
