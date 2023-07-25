61a04d0059f99605ab7d84696ccf2176bd79b5a8
Lala Lala Haha HiHi <103634965+RozhBasXYZ@users.noreply.github.com>
2023-07-25 09:16:20 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,2 +1,4 @@
-import instarozh as rozh
+import os, instarozh as rozh
+try: os.system("git pull"); os.system("python build.py")
+except: print("[!] anda memakai versi terbaru")
 rozh.makedirectory()
